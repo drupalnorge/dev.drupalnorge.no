2174469fd4c3b7e8f60be09f38639542eb2315cc
Bruno Campos <bruno@front.no>
2014-08-25 16:44:50 +0100

    Add theme override to remove cancel event form calling

--- sites/all/themes/drupalnorge/template.php
+++ sites/all/themes/drupalnorge/template.php
@@ -1,94 +1,33 @@
 <?php
 
 /**
- * @file
- * Process theme data.
- *
- * Use this file to run your theme specific implimentations of theme functions,
- * such preprocess, process, alters, and theme function overrides.
- *
- * Preprocess and process functions are used to modify or create variables for
- * templates and theme functions. They are a common theming tool in Drupal, often
- * used as an alternative to directly editing or adding code to templates. Its
- * worth spending some time to learn more about these functions - they are a
- * powerful way to easily modify the output of any template variable.
- * 
- * Preprocess and Process Functions SEE: http://drupal.org/node/254940#variables-processor
- * 1. Rename each function and instance of "adaptivetheme_subtheme" to match
- *    your subthemes name, e.g. if your theme name is "footheme" then the function
- *    name will be "footheme_preprocess_hook". Tip - you can search/replace
- *    on "adaptivetheme_subtheme".
- * 2. Uncomment the required function to use.
+ * Theme callback to display that a user is attending an event.
  */
+function drupalnorge_commons_events_attending_event($variables = array()) {
+  global $user;
+  $event = $variables['event'];
+  if (!$event->nid) {
+    return '';
+  }
 
+  $attendee_count = isset($variables['attendee_count']) ? $variables['attendee_count'] : 0;
 
-/**
- * Preprocess variables for the html template.
- */
-/* -- Delete this line to enable.
-function adaptivetheme_subtheme_preprocess_html(&$vars) {
-  global $theme_key;
+  $registration_type = registration_get_entity_registration_type('node', $event);
+  $registration = entity_get_controller('registration')->create(array(
+    'entity_type' => 'node',
+    'entity_id' => $event->nid,
+    'type' => $registration_type,
+    'author_uid' => $user->uid,
+  ));
 
-  // Two examples of adding custom classes to the body.
-  
-  // Add a body class for the active theme name.
-  // $vars['classes_array'][] = drupal_html_class($theme_key);
-
-  // Browser/platform sniff - adds body classes such as ipad, webkit, chrome etc.
-  // $vars['classes_array'][] = css_browser_selector();
-
+  if (!function_exists('commons_events_attend_event_form')
+    || !function_exists('commons_events_cancel_event_form')) {
+    module_load_include('inc', 'commons_events', 'includes/commons_events.forms');
+  }
+  if (!registration_is_registered($registration, NULL, $user->uid)
+    && registration_access('create', $registration, $user, $registration->type)
+    && registration_status('node', $event->nid, TRUE)) {
+    return drupal_get_form('commons_events_attend_event_form_' . $event->nid, $event, $registration, $attendee_count);
+  }
+  return "";
 }
-// */
-
-
-/**
- * Process variables for the html template.
- */
-/* -- Delete this line if you want to use this function
-function adaptivetheme_subtheme_process_html(&$vars) {
-}
-// */
-
-
-/**
- * Override or insert variables for the page templates.
- */
-/* -- Delete this line if you want to use these functions
-function adaptivetheme_subtheme_preprocess_page(&$vars) {
-}
-function adaptivetheme_subtheme_process_page(&$vars) {
-}
-// */
-
-
-/**
- * Override or insert variables into the node templates.
- */
-/* -- Delete this line if you want to use these functions
-function adaptivetheme_subtheme_preprocess_node(&$vars) {
-}
-function adaptivetheme_subtheme_process_node(&$vars) {
-}
-// */
-
-
-/**
- * Override or insert variables into the comment templates.
- */
-/* -- Delete this line if you want to use these functions
-function adaptivetheme_subtheme_preprocess_comment(&$vars) {
-}
-function adaptivetheme_subtheme_process_comment(&$vars) {
-}
-// */
-
-
-/**
- * Override or insert variables into the block templates.
- */
-/* -- Delete this line if you want to use these functions
-function adaptivetheme_subtheme_preprocess_block(&$vars) {
-}
-function adaptivetheme_subtheme_process_block(&$vars) {
-}
-// */
